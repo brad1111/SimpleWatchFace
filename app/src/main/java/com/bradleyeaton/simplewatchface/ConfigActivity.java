6b623b0613fc20c0672612046296d453d2816f82
Bradley Eaton <mail.thebradad1111@gmail.com>
2020-08-11 11:06:13 +0100

    added a color picking library

--- app/src/main/java/com/bradleyeaton/simplewatchface/ConfigActivity.java
+++ app/src/main/java/com/bradleyeaton/simplewatchface/ConfigActivity.java
@@ -1,5 +1,6 @@
 package com.bradleyeaton.simplewatchface;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
+import org.jraf.android.androidwearcolorpicker.ColorPickActivity;
+import org.jraf.android.androidwearcolorpicker.R.id.*;
 
 public class ConfigActivity extends WearableActivity {
 
@@ -16,11 +19,13 @@ public class ConfigActivity extends WearableActivity {
 
     private Preferences preferences;
 
-    private NumberPicker mPickerRed;
-    private NumberPicker mPickerGreen;
-    private NumberPicker mPickerBlue;
+//    private NumberPicker mPickerRed;
+//    private NumberPicker mPickerGreen;
+//    private NumberPicker mPickerBlue;
 
 //    private Button mButtonSave;
+
+    private static final int REQUEST_PICK_COLOR = 202;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,31 +40,53 @@ public class ConfigActivity extends WearableActivity {
 
 
         //Setup number picker values
-        mPickerRed = (NumberPicker) findViewById(R.id.picker_red);
-        mPickerGreen = (NumberPicker) findViewById(R.id.picker_green);
-        mPickerBlue = (NumberPicker) findViewById(R.id.picker_blue);
+//        mPickerRed = (NumberPicker) findViewById(R.id.picker_red);
+//        mPickerGreen = (NumberPicker) findViewById(R.id.picker_green);
+//        mPickerBlue = (NumberPicker) findViewById(R.id.picker_blue);
 //        mButtonSave = (Button) findViewById(R.id.btn_save);
 
-        mPickerRed.setMinValue(0);
-        mPickerRed.setMaxValue(255);
-
-        mPickerGreen.setMinValue(0);
-        mPickerGreen.setMaxValue(255);
-
-        mPickerBlue.setMinValue(0);
-        mPickerBlue.setMaxValue(255);
+//        mPickerRed.setMinValue(0);
+//        mPickerRed.setMaxValue(255);
+//
+//        mPickerGreen.setMinValue(0);
+//        mPickerGreen.setMaxValue(255);
+//
+//        mPickerBlue.setMinValue(0);
+//        mPickerBlue.setMaxValue(255);
 
 
         // Enables Always-on
         setAmbientEnabled();
     }
 
-    public void onBtnSave(View view){
-        int r = mPickerRed.getValue();
-        int g = mPickerGreen.getValue();
-        int b = mPickerBlue.getValue();
-        int color = Color.argb(255, r, g, b);
-        preferences.setSecondColorRGB(color);
-        preferences.setUpdated();
+//    public void onBtnSave(View view){
+//        int r = mPickerRed.getValue();
+//        int g = mPickerGreen.getValue();
+//        int b = mPickerBlue.getValue();
+//        int color = Color.argb(255, r, g, b);
+//        preferences.setSecondColorRGB(color);
+//        preferences.setUpdated();
+//    }
+
+    public void onBtnSecondsHandColor(View view){
+        //show color picker
+        Intent intent = new ColorPickActivity.IntentBuilder()
+                            .oldColor(preferences.getSecondColor())
+                            .build(this);
+        startActivityForResult(intent, REQUEST_PICK_COLOR);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_PICK_COLOR:
+                if(resultCode == RESULT_OK){
+                    int color = ColorPickActivity.Companion.getPickedColor(data);
+                    preferences.setSecondColorRGB(color);
+                    preferences.setUpdated();
+                }
+                break;
+        }
     }
 }
