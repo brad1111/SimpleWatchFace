686d9bb4ff9fe255ecc48e2f7571ed06c3affa91
Bradley Eaton <mail.thebradad1111@gmail.com>
2020-09-05 11:23:51 +0100

    Setup foreground color to change to black when background color is too light

--- app/src/main/java/com/bradleyeaton/simplewatchface/config/ListItem.java
+++ app/src/main/java/com/bradleyeaton/simplewatchface/config/ListItem.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
+import android.graphics.*;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ScaleDrawable;
+import android.util.Log;
 import android.widget.ImageView;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.ColorUtils;
 import com.bradleyeaton.simplewatchface.R;
 import org.xmlpull.v1.XmlPullParser;
 
@@ -18,8 +19,6 @@ import java.util.Random;
 import java.util.concurrent.Future;
 
 public class ListItem {
-
-    
     private String label;
     private Drawable foreground;
     private Drawable background;
@@ -55,7 +54,7 @@ public class ListItem {
         }
         final int FOREGROUND_DIAMETER = 140; //24px
 
-        this.foreground = new ScaleDrawable(foregroundUnscaled, 0, FOREGROUND_DIAMETER, FOREGROUND_DIAMETER).getDrawable();
+        this.foreground = new ScaleDrawable(foregroundUnscaled, 0, FOREGROUND_DIAMETER, FOREGROUND_DIAMETER).getDrawable().mutate();
         this.foreground.setBounds(0,0,FOREGROUND_DIAMETER,FOREGROUND_DIAMETER);
         this.background = ResourcesCompat.getDrawable(res, R.drawable.circle, context.getTheme()).mutate();
         this.background.setTintList(ColorStateList.valueOf(tintColor));
@@ -85,6 +84,20 @@ public class ListItem {
     public void setBackgroundColor(int tintColor){
         this.tintColor = tintColor;
         background.setTintList(ColorStateList.valueOf(tintColor));
+        setForegroundColor();
+    }
+
+    private void setForegroundColor(){
+        float[] hsl = new float[3];
+        ColorUtils.colorToHSL(tintColor, hsl);
+        if(hsl[2] > 0.8){
+            //Too light background then switch foreground to a dark color
+            foreground.setColorFilter(new PorterDuffColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY));
+        } else {
+            if(foreground.getColorFilter() != null){
+                foreground.setColorFilter(null);
+            }
+        }
     }
 
     public int getRequestCode() {
